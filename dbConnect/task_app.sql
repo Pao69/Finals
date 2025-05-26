fe6b76f7c57984581c5707f7be00dff66bad43f1
Pao69 <paolovaldez69@gmail.com>
2025-05-26 17:21:44 +0800

    Update task_app.sql to reflect new data entries and adjust AUTO_INCREMENT values for several tables. Modify generation time for clarity and add multiple new records for password resets, resources, tasks, and users.

--- dbConnect/task_app.sql
+++ dbConnect/task_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 26, 2025 at 05:12 AM
+-- Generation Time: May 26, 2025 at 11:19 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -69,7 +69,21 @@ INSERT INTO `password_resets` (`id`, `email`, `code`, `expiry`, `used`, `created
 (14, 'shaiyon@gmail.com', '382928', '2025-05-24 22:51:47', 0, '2025-05-25 04:41:47'),
 (15, 'shaiyon@gmail.com', '469386', '2025-05-24 22:57:59', 0, '2025-05-25 04:47:59'),
 (16, 'shaiyon@gmail.com', '340040', '2025-05-24 23:02:07', 0, '2025-05-25 04:52:07'),
-(17, 'shaiyon@gmail.com', '409477', '2025-05-25 00:09:16', 0, '2025-05-25 05:59:16');
+(17, 'shaiyon@gmail.com', '409477', '2025-05-25 00:09:16', 0, '2025-05-25 05:59:16'),
+(18, 'shaiyon@gmail.com', '367036', '2025-05-26 01:13:35', 0, '2025-05-26 07:03:35'),
+(19, 'shaiyon@gmail.com', '213699', '2025-05-26 01:13:45', 0, '2025-05-26 07:03:45'),
+(20, 'shaiyon@gmail.com', '290002', '2025-05-26 01:15:02', 0, '2025-05-26 07:05:02'),
+(21, 'rumofart@gmail.com', '225235', '2025-05-26 01:17:32', 0, '2025-05-26 07:07:32'),
+(22, 'rumofart@gmail.com', '924173', '2025-05-26 01:19:15', 0, '2025-05-26 07:09:15'),
+(23, 'rumofart@gmail.com', '788170', '2025-05-26 01:22:11', 0, '2025-05-26 07:12:11'),
+(24, 'rumofart@gmail.com', '763646', '2025-05-26 01:51:46', 0, '2025-05-26 07:41:46'),
+(25, 'rumofart@gmail.com', '927876', '2025-05-26 01:56:43', 0, '2025-05-26 07:46:43'),
+(26, 'rumofart@gmail.com', '584949', '2025-05-26 01:59:09', 0, '2025-05-26 07:49:09'),
+(27, 'rumofart@gmail.com', '622181', '2025-05-26 02:00:18', 0, '2025-05-26 07:50:18'),
+(28, 'rumofart@gmail.com', '279043', '2025-05-26 02:02:09', 0, '2025-05-26 07:52:09'),
+(29, 'rumofart@gmail.com', '426876', '2025-05-26 02:04:10', 0, '2025-05-26 07:54:10'),
+(30, 'rumofart@gmail.com', '856434', '2025-05-26 07:57:01', 1, '2025-05-26 07:56:51'),
+(31, 'rumofart@gmail.com', '236585', '2025-05-26 07:59:15', 1, '2025-05-26 07:58:35');
 
 -- --------------------------------------------------------
 
@@ -95,7 +109,8 @@ CREATE TABLE `resources` (
 
 INSERT INTO `resources` (`id`, `user_id`, `task_id`, `filename`, `original_filename`, `file_type`, `file_size`, `description`, `upload_date`) VALUES
 (6, 16, 18, 'resource_6832a62199af6.jpg', '494179745_954918206850069_5988527051095703533_n - Copy.jpg', 'image/jpeg', 27992, 'Skbidi', '2025-05-25 13:09:53'),
-(7, 16, 21, 'resource_6833d34bc219e.jpg', '494688483_1508759380085143_2311175477849458383_n.jpg', 'image/jpeg', 126185, 'f', '2025-05-26 10:34:51');
+(7, 16, 21, 'resource_6833d34bc219e.jpg', '494688483_1508759380085143_2311175477849458383_n.jpg', 'image/jpeg', 126185, 'f', '2025-05-26 10:34:51'),
+(9, 17, 27, 'resource_68340f078cbae.docx', 'Chap 1-5.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 598182, 'Task resources to read', '2025-05-26 14:49:43');
 
 -- --------------------------------------------------------
 
@@ -120,7 +135,7 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `user_id`, `title`, `description`, `due_date`, `completed`, `created_at`, `updated_at`, `priority`) VALUES
-(7, 9, 'f', 'f', '2025-05-19 00:00:00', 0, '2025-05-19 00:46:36', '2025-05-19 00:46:36', 'medium'),
+(7, 9, 'f', 'f', '2025-05-19 00:00:00', 0, '2025-05-19 00:46:36', '2025-05-26 12:44:31', 'low'),
 (8, 9, 'skbidi', 'fffwa', '2025-05-19 00:00:00', 0, '2025-05-19 18:26:36', '2025-05-19 18:26:36', 'medium'),
 (9, 10, 'Skibidi', 'EAT MY ASS', '2025-05-30 00:00:00', 1, '2025-05-19 20:55:04', '2025-05-24 16:03:00', 'medium'),
 (10, 10, 'Rape Eaton', 'eaton rape spread the word', '2025-05-27 00:00:00', 1, '2025-05-24 15:46:23', '2025-05-24 16:02:44', 'medium'),
@@ -131,7 +146,10 @@ INSERT INTO `tasks` (`id`, `user_id`, `title`, `description`, `due_date`, `compl
 (21, 16, 'tae', 'tae', '2025-05-26 10:30:00', 0, '2025-05-26 10:30:46', '2025-05-26 10:30:46', 'medium'),
 (22, 16, 'aafwwaggg', 'ffawfawgg', '2025-08-09 17:09:00', 0, '2025-05-26 10:44:36', '2025-05-26 10:44:36', 'medium'),
 (23, 16, 'tae', 'tae', '2025-05-26 02:36:00', 0, '2025-05-26 10:56:39', '2025-05-26 10:56:39', 'medium'),
-(24, 16, 'tae', 'tae', '2025-06-07 18:36:00', 1, '2025-05-26 10:56:47', '2025-05-26 10:56:47', 'medium');
+(24, 16, 'tae', 'tae', '2025-06-07 18:36:00', 1, '2025-05-26 10:56:47', '2025-05-26 10:56:47', 'medium'),
+(25, 16, 'rumonide', 'djwkaljlksdklsaj', '2025-06-09 04:53:00', 0, '2025-05-26 12:53:41', '2025-05-26 12:53:41', 'medium'),
+(26, 16, 'rumonide', 'djwkaljlksdklsaj', '2025-06-08 12:53:00', 0, '2025-05-26 12:53:53', '2025-05-26 12:56:51', 'high'),
+(27, 17, 'Task today', 'do this task asap', '2025-05-27 06:42:00', 0, '2025-05-26 14:43:09', '2025-05-26 14:43:09', 'high');
 
 -- --------------------------------------------------------
 
@@ -163,8 +181,9 @@ INSERT INTO `users` (`id`, `username`, `password`, `phone`, `email`, `last_login
 (8, 'Tae', '$2y$10$JLVy5ZEqkuAGARK.IXwFcORuI7OTEB1Tn4EjOdPptY2A/T4aESzhC', '09999999995', 'tae@gmail.com', NULL, 'user', '2025-05-18 18:45:14', NULL),
 (9, 'MongoLloyd', '$2y$10$Fb1IJs37dvrlCOK8zh4wwOTzY3ZJgVyN3EfDtMAlD/ECBIi9HakSu', '09999999994', 'munggoNiLloyd@gmail.com', '2025-05-19 19:26:46', 'user', '2025-05-18 18:45:14', NULL),
 (10, 'Shai', '$2y$10$ePO1XS5CCfLZT2JxSPb2yebfQiqecNC1yMSTwUqNbUIIcp6b.BxUW', '09545454545', 'shaiyon@gmail.com', '2025-05-24 17:40:39', 'user', '2025-05-19 19:32:56', '/src/images/profile_10_1748079664.jpg'),
-(11, 'Rumonite', '$2y$10$gfld3RXcDuaJnc.7GhAaUuE.WVzL0R74oDsYj7dKrk6qxzmA2phDe', '09812390823', 'rumofart@gmail.com', '2025-05-26 09:27:55', 'admin', '2025-05-24 16:44:32', '/pfp/profile_11_1748221377.jpg'),
-(16, 'Jerome', '$2y$10$c8TV7b4hlEiHidw0W3ap4.95tRnJ0Zm05EVOc.hgneYH/mkEA2oOy', '63478962349', 'Jerome@gmail.com', '2025-05-26 11:10:55', 'user', '2025-05-25 13:07:24', '/pfp/profile_16_1748227453.png');
+(11, 'Rumonite', '$2y$10$/dcGhquHKKwizV5Pz0Ef0udRk.kTpW8R111uiEaEDjiAhtHIdjIsy', '09812390823', 'rumofart@gmail.com', '2025-05-26 15:59:23', 'admin', '2025-05-24 16:44:32', NULL),
+(16, 'Jerome', '$2y$10$c8TV7b4hlEiHidw0W3ap4.95tRnJ0Zm05EVOc.hgneYH/mkEA2oOy', '63478962349', 'Jerome@gmail.com', '2025-05-26 14:47:26', 'user', '2025-05-25 13:07:24', '/pfp/profile_16_1748227453.png'),
+(17, 'useraccount', '$2y$10$Rvf8BBL.0dahI8S.4biNh.6Ldn3yfVVbccZcuMHuuyEXbpuvTlrH2', '09123451234', 'useracc@gmail.com', '2025-05-26 14:47:47', 'user', '2025-05-26 14:38:55', NULL);
 
 --
 -- Indexes for dumped tables
@@ -225,25 +244,25 @@ ALTER TABLE `password_attempts`
 -- AUTO_INCREMENT for table `password_resets`
 --
 ALTER TABLE `password_resets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `resources`
 --
 ALTER TABLE `resources`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Constraints for dumped tables
